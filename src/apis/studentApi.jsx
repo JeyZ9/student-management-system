e007b1ea9d4a3c98f6c0efcd0683c661296cca85
JeyZ9 <ggjj4511@gmail.com>
2025-03-09 14:10:08 +0700

    change api from fetchStudents to searchStudent and debug method ueffect

--- src/apis/studentApi.jsx
+++ src/apis/studentApi.jsx
@@ -6,26 +6,33 @@ const api = axios.create({
 });
 
 export const fetchStudents = async () => {
-    const response = await api.get('student');
+    const response = await api.get('students');
+    return response.data;
+}
+export const searchStudents = async (keyword, page, size) => {
+    const response = await api.get(`/students/search?keyword=${keyword}&page=${page}&size=${size}`);
+    console.log("Response Data: ", response.data);
+    console.log("Students:", response.data.students);
+
     return response.data;
 }
 
 export const fetchStudentById = async (studentId) => {
-    const response = await api.get(`student/${studentId}`);
+    const response = await api.get(`/students/${studentId}`);
     return response.data;
 }
 
 export const addStudent = async (studentData) => {
-    const response = await api.post('student', studentData);
+    const response = await api.post('/students/create', studentData);
     return response.data
 }
 
-export const updateStudent = async (studentData) => {
-    const response = await api.put('student', studentData);
+export const updateStudent = async (studentId, studentData) => {
+    const response = await api.put(`/students/update/${studentId}`, studentData);
     return response.data
 }
 
 export const removeStudent = async (studentId) => {
-    const response = await api.delete(`student/${studentId}`);
+    const response = await api.delete(`/students/delete/${studentId}`);
     return response.data
 }
--- src/pages/Management.jsx
+++ src/pages/Management.jsx
@@ -1,50 +1,180 @@
-import React, { useEffect, useState } from 'react';
-import { fetchStudentById, fetchStudents, removeStudent } from '../apis/studentApi';
+import React, { useCallback, useEffect, useState } from "react";
+import {
+  searchStudents,
+  removeStudent,
+} from "../apis/studentApi";
+import Swal from "sweetalert2";
+import { FaRegEdit } from "react-icons/fa";
+import { RiDeleteBin5Line } from "react-icons/ri";
+import { IoSearch } from "react-icons/io5";
+import { z } from "zod";
 
 const Management = () => {
-
   const [ student, setStudent ] = useState([]);
-  const [ inputValue, setInputValue ] = useState('');
+  const [ inputValue, setInputValue ] = useState("");
+  const [controls, setControls] = useState({ page: 0, size: 10, total: 0 });
+
+
+  const controlsSchema = z.object({
+    page: z.number().min(0),
+    size: z.number().min(1).max(10),
+    total: z.number().min(0)
+  })
+
+  const controllers = {
+    get: controls,
+    set: (state, value ) => 
+      setControls((prev) => ({ ...prev, [state]: value}))
+  }
+
+  const validateControls = () => {
+    try{
+      controlsSchema.parse(controls);
+    } catch(error){
+      if(error instanceof z.ZodError){
+        console.log("Validation Error: ", error.errors);
+      }
+    }
+  }
+
+  // const handleSubmit = useCallback((e) => {
+  //   e.preventDefault();
+  // }, []);
+
+  const handleChange = useCallback((e) => {
+    setInputValue(e.target.value);
+  }, []);
+
+  const handleSubmit = useCallback((e) => {
+    e.preventDefault();
+    searchStudents(inputValue, controllers.get.page, controllers.get.size)
+      .then((data) => {
+        setStudent(data.students);
+        controllers.set("total", data.total);
+      })
+      .catch(console.error);
+    
+    // setInputValue("");
+  }, [inputValue, controllers.get.page, controllers.get.size]);
 
   useEffect(() => {
-    fetchStudents().then(setStudent).catch(console.error)
-  }, [])
+    validateControls();
+    searchStudents("", controllers.get.page, controllers.get.size)
+      .then((data) => {
+        setStudent(data.students);
+        controllers.set("total", data.total);
+      })
+      .catch(console.error);
+  }, [controllers.get.page, controllers.get.size])
+
+  // useEffect(() => {
+  //   let isMounted = true;
+  //   // searchStudents("", controllers.get.page, controllers.get.size)
+  //   //   .then((data) => {
+  //   //     if (data && Array.isArray(data.students)) {
+  //   //       console.log("Log in useEffect", data.students);
+  //   //       setStudent(data.students);
+  //   //       controllers.set("total", data.total);
+  //   //     } else {
+  //   //       console.error("No students found in the response:", data);
+  //   //       setStudent([]);
+  //   //       controllers.set("total", 0);
+  //   //     }
+  //   //   })
+  //   //   .catch((error) => {
+  //   //     console.error("Error fetching students:", error);
+  //   //     setStudent([]);
+  //   //     controllers.set("total", 0);
+  //   //   });
+
+
+  //   // if (inputValue === "") {
+  //   //   searchStudents("", controllers.get.page, controllers.get.size)
+  //   //     .then((data) => {
+  //   //       setStudent(data.students);
+  //   //       // controllers.set("total", data.students.length);
+  //   //       controllers.set("total", data.total);
+  //   //     })
+  //   //     .catch(console.error);
+  //   // } else {
+  //   //   searchStudents(inputValue, controllers.get.page, controllers.get.size)
+  //   //     .then((data) => {
+  //   //       // const filteredStudents = data.students.filter(
+  //   //       //   (user) =>
+  //   //       //     user.id.includes(inputValue) ||
+  //   //       //     user.name.toLowerCase().includes(inputValue.toLowerCase())
+  //   //       // );
+  //   //       controllers.set("total", data.total);
+  //   //       setStudent(data.students);
+  //   //     })
+  //   //     .catch(console.error);
+  //   // }
+
+  //   const findStudent = async() => {
+  //     try{
+  //       const data = await searchStudents(inputValue, controllers.get.page, controllers.get.size);
+  //       if(isMounted){
+  //         setStudent(data.students);
+  //         controllers.set("total", data.total);
+  //       }
+  //     }catch(e){
+  //       console.error("Error find Student: ", e);
+  //       if(isMounted){
+  //         setStudent([]);
+  //         controllers.set("total", 0);
+  //       }
+  //     }
+  //   }
+  //   findStudent();
+  //   return () => {
+  //     isMounted = false;
+  //   }
+  // }, [controllers.get.page, controllers.get.size, inputValue]);
+  
 
   console.log(student);
 
   const deleteStudent = (studentId) => {
-    removeStudent(studentId).then(() => {setStudent(student.filter(user => user.id !== studentId))}).catch(console.error);
-  }
-
-  const handleChange = (e) => {
-    setInputValue(e.target.value);
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (inputValue === "") {
-      fetchStudents().then(setStudent).catch(console.error);
-    } else {
-      fetchStudents().then((students) => {
-        const filteredStudents = students.filter(user =>
-          user.id.includes(inputValue) || 
-          user.name.toLowerCase().includes(inputValue.toLowerCase())
-        );
-        setStudent(filteredStudents);
-      }).catch(console.error);
-    }
-  }
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: "Deleted!",
+          text: "Student has been deleted.",
+          icon: "success",
+        });
+        removeStudent(studentId)
+          .then(() => {
+            setStudent(student.filter((user) => user.id !== studentId));
+          })
+          .catch(console.error);
+      }
+    });
+  };
 
   return (
-    <div className='p-4'>
+    <div className="flex flex-col px-96 gap-2 py-14">
       <form className="flex justify-between" onSubmit={handleSubmit}>
-        <h1>Students List</h1>
-        <div className="">
-          <input type="text" className='border' value={inputValue} onChange={handleChange} />
-          <button type='submit'>S</button>
+        <h1 className="text-2xl font-semibold text-gray-600">Students List</h1>
+        <div className="h-[30px]">
+          <input
+            type="text"
+            className="border h-full rounded-l-sm px-2 outline-green-500/50 text-gray-700"
+            value={inputValue}
+            onChange={handleChange}
+            placeholder="Secrch..."
+          />
+          <button type="submit" className="p-2 h-full bg-green-500 hover:bg-green-600 text-white ease-in-out delay-200 duration-200 rounded-r-sm">{ <IoSearch /> }</button>
         </div>
       </form>
-      <table className='border-collapse border border-gray-400 w-full'>
+      {/* <table className='border-collapse border border-gray-400 max-w-full'>
         <thead>
           <tr className='bg-gray-200'>
             <th className='border border-gray-400 p-2'>Student ID</th>
@@ -58,15 +188,90 @@ const Management = () => {
               <tr key={user.id}>
                 <td className='border border-gray-400 p-2'>{ user.id }</td>
                 <td className='border border-gray-400 p-2'>{ user.name }</td>
-                <td className='border border-gray-400 p-2'>
-                    <button>Edit</button>
-                    <button onClick={() => deleteStudent(user.id)}>Delete</button>
+                <td className='border border-gray-400 p-2 text-center '>
+                    <button className='text-white p-2 bg-yellow-500 rounded-sm hover:bg-yellow-600 ease-in-out delay-200 duration-200 mx-1'>{ <FaRegEdit /> }</button>
+                    <button onClick={() => deleteStudent(user.id)} className='text-white p-2 bg-red-500 rounded-sm hover:bg-red-600 ease-in-out delay-200 duration-200 mx-1'>{ <RiDeleteBin5Line /> }</button>
                   </td>
               </tr>
             )
           })}
         </tbody>
-      </table>
+      </table> */}
+
+      <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50">
+            <tr className="bg-gray-200">
+              <th className="border border-gray-400 p-2">Student ID</th>
+              <th className="border border-gray-400 p-2">Name</th>
+              <th className="border border-gray-400 p-2">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {student.map((user) => {
+              return (
+                <tr key={user.id}>
+                  <td className="border border-gray-400 p-2">{user.id}</td>
+                  <td className="border border-gray-400 p-2">{user.name}</td>
+                  <td className="border border-gray-400 p-2 text-center ">
+                    <button className="text-white p-2 bg-yellow-500 rounded-sm hover:bg-yellow-600 ease-in-out delay-200 duration-200 mx-1">
+                      {<FaRegEdit />}
+                    </button>
+                    <button
+                      onClick={() => deleteStudent(user.id)}
+                      className="text-white p-2 bg-red-500 rounded-sm hover:bg-red-600 ease-in-out delay-200 duration-200 mx-1"
+                    >
+                      {<RiDeleteBin5Line />}
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+        <nav
+          className="flex items-center flex-column flex-wrap md:flex-row justify-between pt-4"
+          aria-label="Table navigation"
+        >
+          <span className="text-sm font-normal text-gray-500 mb-4 md:mb-0 block w-full md:inline md:w-auto">
+            Showing&nbsp;
+            <span className="font-semibold text-gray-600">
+              {controllers.get.page + 1}
+            </span>&nbsp;of&nbsp;
+            <span className="font-semibold text-gray-600">
+              {Math.ceil(controllers.get.total / controllers.get.size)}
+            </span>
+          </span>
+          <ul className="inline-flex -space-x-px rtl:space-x-reverse text-sm h-8">
+            <li>
+              <button onClick={() => controllers.set("page", controllers.get.page - 1)} disabled={controllers.get.page <= 0} className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700">
+                Previous
+              </button>
+            </li>
+
+            {[...Array(Math.ceil(controllers.get.total / controllers.get.size))].map((_, index) => (
+              <li key={index}>
+                <button
+                  onClick={() => controllers.set('page', index)}
+                  className={`flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 ${
+                    controllers.get.page === index ? 'bg-green-500/50 text-white' : ''
+                  }`}
+                >
+                  {index + 1}
+                </button>
+              </li>
+            ))}
+
+            <li>
+              <button onClick={() => controllers.set("page", controllers.get.page + 1)} disabled={controllers.get.page + 1 >= (Math.ceil(controllers.get.total / controllers.get.size)) || controllers.get.total > 100} className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700">
+                Next
+              </button>
+            </li>
+          </ul>
+        </nav>
+
+      </div>
+
     </div>
   );
 };
